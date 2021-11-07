7f8e9ddba35b65392987e16942feea4be7950703
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-11-07 22:25:32 +0530

    add comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         	System.out.println("You entered: " + number);
     }
 }
-//program of 
+//program of addition
 class Main {
 
   public static void main(String[] args) {
