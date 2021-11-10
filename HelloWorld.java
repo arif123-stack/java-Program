64298cd9d02b0ebdd1a99dcd780ff823a29a5bcc
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-11-10 23:02:53 +0530

    add program

--- HelloWorld.java
+++ HelloWorld.java
@@ -24,3 +24,23 @@ class Main {
     System.out.println("The sum is: " + sum);
   }
 }
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 29;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
