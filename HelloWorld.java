8a83cd4424134bc3c5e54c3a3d028a5b43713368
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-11-13 23:02:28 +0530

    add program

--- HelloWorld.java
+++ HelloWorld.java
@@ -46,3 +46,17 @@ public class Main {
       System.out.println(num + " is not a prime number.");
   }
 }
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
+
