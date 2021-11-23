8401eda5eb5571fdba861bf602b89a827840468a
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-11-23 23:23:17 +0530

    edit in program

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,8 +14,8 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
+    int first = 30;
+    int second = 50;
     
     System.out.println(first + " " + second);
 
